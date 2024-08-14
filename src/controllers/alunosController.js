acc3a97770073dbfefb189fcd478aa42e50451cd
Luciano H. Ribeiro <luciano@scopi.com.br>
2024-08-14 20:53:39 -0300

    CRUD alunos

--- src/controllers/alunosController.js
+++ src/controllers/alunosController.js
@@ -1,11 +1,71 @@
 import { connection } from '../database/mysqlConnection.js';
 
 class AlunosController {
-  async listarAlunos(request, response) {
-    console.log(`GET /alunos`);
-    const [results] = await connection.query('SELECT * FROM alunos');
+  async listar(request, response) {
+    console.log('GET /alunos');
+    const [results] = await connection.query('SELECT codigo, nome, cpf, matricula, nascimento FROM alunos');
     response.json(results);
-  }
+  };
+
+  async inserir(request, response) {
+    console.log('POST /alunos');
+    console.log(request.body);
+    const query = 'INSERT INTO alunos (nome, cpf, matricula, nascimento) VALUES (?, ?, ?, ?)';
+    try {
+      await connection.query(query, [
+        request.body.nome,
+        request.body.cpf,
+        request.body.matricula,
+        request.body.nascimento
+      ]);
+      response.status(201).send();
+    } catch (error) {
+      console.log(error);
+      response.status(400).send(error.message);
+    }
+  };
+
+  async atualizar(request, response) {
+    console.log(`PUT /alunos/${request.params.codigo}`);
+    console.log(request.body);
+    let fields = [];
+    if (request.body.nome) fields.push(`nome = "${request.body.nome}"`)
+    if (request.body.cpf) fields.push(`cpf = "${request.body.cpf}"`)
+    if (request.body.matricula) fields.push(`matricula = "${request.body.matricula}"`)
+    if (request.body.nascimento) fields.push(`nascimento = "${request.body.nascimento}"`)
+    const query = `UPDATE alunos SET ${fields.join(', ')} WHERE codigo = ?`;
+    try {
+      await connection.query(query, [request.params.codigo]);
+      response.status(200).send();
+    } catch (error) {
+      console.log(error);
+      response.status(400).send(error.message);
+    }
+  };
+
+  async excluir(request, response) {
+    console.log(`DELETE /alunos/${request.params.codigo}`);
+    const query = `DELETE FROM alunos WHERE codigo = ?`;
+    try {
+      await connection.query(query, [request.params.codigo]);
+      response.status(204).send();
+    } catch (error) {
+      console.log(error);
+      response.status(400).send(error.message);
+    }
+  };
+
+  async exibir(request, response) {
+    console.log(`GET /alunos/${request.params.codigo}`);
+    const query = `SELECT codigo, nome, cpf, matricula, nascimento FROM alunos WHERE codigo = ?`;
+    try {
+      const [result] = await connection.query(query, [request.params.codigo]);
+      response.json(result[0]);
+    } catch (error) {
+      console.log(error);
+      response.status(400).send(error.message);
+    }
+  };
 }
 
 export default new AlunosController();
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -4,6 +4,10 @@ import AlunosController from "../controllers/alunosController.js";
 
 const router = express.Router();
 
-router.get('/alunos', AlunosController.listarAlunos);
+router.get('/alunos', AlunosController.listar);
+router.post('/alunos', AlunosController.inserir);
+router.put('/alunos/:codigo', AlunosController.atualizar);
+router.delete('/alunos/:codigo', AlunosController.excluir);
+router.get('/alunos/:codigo', AlunosController.exibir);
 
 export default router;
